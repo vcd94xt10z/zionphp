ace0cb9d3034ddf08ef9cb4cec70074dcd4bcaed
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-02-01 13:42:53 -0200

    .

--- modules/user/view/user-loginform.php
+++ modules/user/view/user-loginform.php
@@ -4,30 +4,46 @@
     <title>Login</title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <!-- styles -->
+    <link href="/zion/lib/bootstrap-4.2.1-dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="/zion/mod/user/view/css/user-loginform.css">
+	<!-- styles -->
 </head>
 <body>
-	<div class="container-center">
-    	<div id="login-box">
-        	<div class="login-logo">
-        		<img src="/zion/mod/user/view/img/logo.png">
-        	</div>
-        	<div>
-        		<label class="user-label">Usuário</label>
-        	</div>
-        	<div>
-        		<input id="user-login" class="user-input">
-        	</div>
-        	<div>
-        		<label class="user-label">Senha</label>
-        	</div>
-        	<div>
-        		<input id="user-password" class="user-input">
-        	</div>
-        	<div>
-        		<button class="user-button">Entrar</button>
-        	</div>
-        </div>
+	<div class="rangerow">
+		<div class="center-content">
+			Zion
+		</div>
 	</div>
+	
+	<div class="container-fluid">
+    	<div id="zlogin" class="center-content">
+    		Login
+    		
+    		<div class="row">
+    			<div class="col-12">
+    				<label class="user-label">Usuário</label>
+    			</div>
+    			<div class="col-12">
+    				<input id="user-login" class="user-input">
+    			</div>
+    			<div class="col-12">
+    				<label class="user-label">Senha</label>
+    			</div>
+    			<div class="col-12">
+    				<input id="user-password" class="user-input">
+    			</div>
+    			<div class="col-12">
+    				<button class="user-button">Entrar</button>
+    			</div>
+    		</div>
+    		
+    	</div>
+	</div>
+	
+	<!-- scripts -->
+	<script src="/zion/lib/jquery/jquery-3.3.1.min.js"></script>
+	<script src="/zion/lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
+    <!-- scripts -->
 </body>
 </html>
