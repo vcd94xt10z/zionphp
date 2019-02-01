e20bde6beacb4b7092b83f7f5821b1a84899e59e
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-02-01 14:59:04 -0200

    .

--- modules/user/view/user-loginform.php
+++ modules/user/view/user-loginform.php
@@ -5,39 +5,46 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
     <!-- styles -->
-    <link href="/zion/lib/bootstrap-4.2.1-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/zion/lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/zion/mod/user/view/css/user-loginform.css">
 	<!-- styles -->
 </head>
 <body>
-	<div class="rangerow">
-		<div class="center-content">
-			Zion
-		</div>
-	</div>
-	
-	<div class="container-fluid">
-    	<div id="zlogin" class="center-content">
-    		Login
-    		
+
+	<div id="zlogin" class="center-content">
+		<div class="container-fluid">
     		<div class="row">
     			<div class="col-12">
-    				<label class="user-label">Usuário</label>
+    				<div>
+    					<label for="user-login" class="user-label">Usuário</label>
+    				</div>
     			</div>
     			<div class="col-12">
-    				<input id="user-login" class="user-input">
+    				<div>
+    					<input id="user-login" class="user-input" type="text" size="20">
+    				</div>
     			</div>
     			<div class="col-12">
-    				<label class="user-label">Senha</label>
+    				<div>
+    					<label for="user-password" class="user-label">Senha</label>
+    				</div>
     			</div>
     			<div class="col-12">
-    				<input id="user-password" class="user-input">
+    				<div>
+    					<input id="user-password" class="user-input" type="password" size="20">
+    				</div>
     			</div>
     			<div class="col-12">
-    				<button class="user-button">Entrar</button>
+    				<div>
+    					<button type="button" class="user-button">Entrar</button>
+    				</div>
+    			</div>
+    			<div class="col-12">
+    				<div>
+    					<a href="#" class="recoverMyPassword">Esqueci minha senha</a>
+    				</div>
     			</div>
     		</div>
-    		
     	</div>
 	</div>
 	
