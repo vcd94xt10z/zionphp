5b2188565998f4a0ca24650e6bc669669dea02c4
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-02-13 16:57:06 -0200

    .

---
+++ artifacts/dump/99-functions.sql
@@ -0,0 +1,30 @@
+DROP FUNCTION IF EXISTS `zion_nextval`;
+
+DELIMITER $$
+CREATE FUNCTION `zion_nextval`(
+	`seq_name` VARCHAR(100)
+) RETURNS bigint(20)
+BEGIN
+	DECLARE returnval bigint;
+    DECLARE countRow INT;
+	    
+    UPDATE `zion_sequence` 
+	   SET `last_value` = `last_value` + 1
+     WHERE `name` = seq_name;
+		
+	SET countRow = ROW_COUNT();
+	IF(countRow = 0) THEN
+		INSERT INTO `zion_sequence` 
+        (`name`,`last_value`) 
+		VALUES
+		(seq_name,1);
+    END IF;
+ 	
+    SELECT `last_value` INTO returnval
+      FROM `zion_sequence`
+     WHERE `name` = seq_name
+     LIMIT 1;
+     
+    RETURN returnval;
+END$$
+DELIMITER ;
