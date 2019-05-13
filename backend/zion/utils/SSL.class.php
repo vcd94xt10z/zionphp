ed63f433f77497a2dd316273b8df5de553c9d785
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-05-13 17:09:14 -0300

    .

--- backend/zion/utils/SSL.class.php
+++ backend/zion/utils/SSL.class.php
@@ -2,6 +2,7 @@
 namespace zion\utils;
 
 use zion\orm\ObjectVO;
+use zion\core\System;
 
 /**
  * @author Vinicius Cesar Dias
@@ -18,34 +19,11 @@ class SSL {
         $linesScriptSite = array();
         $linesExt        = array();
         $linesVhosts     = array();
+        $linesInfo       = array();
         
         $linesScriptCA[] = "certs_ca.sh";
         $linesScriptCA[] = "#!/bin/bash";
         $linesScriptCA[] = "";
-        
-        // chave privada da CA
-        $linesScriptCA[] = "openssl genrsa -des3 -passout pass:{$obj->get("ca_password")} -out {$obj->get("ca_name")}.key 2048";
-        
-        // certificado root da CA
-        $line  = "openssl req -x509 -new -nodes -key {$obj->get("ca_name")}.key -sha256 -days 36500 ";
-        $line .= "-passin pass:{$obj->get("ca_password")} -subj \"/C={$obj->get("ca_country")}/ST={$obj->get("ca_state")}/L={$obj->get("ca_city")}/O={$obj->get("ca_org")}/CN={$obj->get("ca_domain")}\" ";
-        $line .= "-out {$obj->get("ca_name")}.pem";
-        $linesScriptCA[] = $line;
-        
-        // convertendo para crt para ser instalado no windows
-        $linesScriptCA[] = "openssl x509 -outform der -in {$obj->get("ca_name")}.pem -out {$obj->get("ca_name")}.crt";
-        
-        // chave privada do site
-        $linesScriptSite[] = "certs_site.sh";
-        $linesScriptSite[] = "#!/bin/bash";
-        $linesScriptSite[] = "";
-        $linesScriptSite[] = "openssl genrsa -out {$obj->get("site_domain")}.key 2048";
-        
-        // certificado do site
-        $line  = "openssl req -new -key {$obj->get("site_domain")}.key ";
-        $line .= "-subj \"/C={$obj->get("site_country")}/ST={$obj->get("site_state")}/L={$obj->get("site_city")}/O={$obj->get("site_org")}/CN={$obj->get("site_domain")}\" ";
-        $line .= "-out {$obj->get("site_domain")}.csr";
-        $linesScriptSite[] = $line;
         
         // arquivo de configuração
         $altDNS = explode("\n",$obj->get("site_alt_dns"));
@@ -74,7 +52,30 @@ class SSL {
             $linesExt[] = "IP.{$i} = {$alt}";
             $i++;
         }
-        $linesExt[] = "";
+        
+        // chave privada da CA
+        $linesScriptCA[] = "openssl genrsa -des3 -passout pass:{$obj->get("ca_password")} -out {$obj->get("ca_name")}.key 2048";
+        
+        // certificado root da CA
+        $line  = "openssl req -x509 -new -nodes -key {$obj->get("ca_name")}.key -sha256 -days 36500 ";
+        $line .= "-passin pass:{$obj->get("ca_password")} -subj \"/C={$obj->get("ca_country")}/ST={$obj->get("ca_state")}/L={$obj->get("ca_city")}/O={$obj->get("ca_org")}/CN={$obj->get("ca_domain")}\" ";
+        $line .= "-out {$obj->get("ca_name")}.pem";
+        $linesScriptCA[] = $line;
+        
+        // convertendo para crt para ser instalado no windows
+        $linesScriptCA[] = "openssl x509 -outform der -in {$obj->get("ca_name")}.pem -out {$obj->get("ca_name")}.crt";
+        
+        // chave privada do site
+        $linesScriptSite[] = "certs_site.sh";
+        $linesScriptSite[] = "#!/bin/bash";
+        $linesScriptSite[] = "";
+        $linesScriptSite[] = "openssl genrsa -out {$obj->get("site_domain")}.key 2048";
+        
+        // certificado do site
+        $line  = "openssl req -new -key {$obj->get("site_domain")}.key ";
+        $line .= "-subj \"/C={$obj->get("site_country")}/ST={$obj->get("site_state")}/L={$obj->get("site_city")}/O={$obj->get("site_org")}/CN={$obj->get("site_domain")}\" ";
+        $line .= "-out {$obj->get("site_domain")}.csr";
+        $linesScriptSite[] = $line;
         
         // gerando certificado do site emitido pela CA
         $line  = "openssl x509 -req -in {$obj->get("site_domain")}.csr -CA {$obj->get("ca_name")}.pem ";
@@ -115,11 +116,17 @@ class SSL {
         $linesVhosts[] = "  &lt;/Directory&gt;";
         $linesVhosts[] = "&lt;/VirtualHost&gt;";
         
+        // arquivo de informações
+        $linesInfo[] = "info.txt";
+        $linesInfo[] = "created ".date("d/m/Y H:i:s")." ".System::get("timezone");
+        $linesInfo[] = "ca password = ".$obj->get("ca_password");
+        
         return array(
             "scriptCA"   => implode("\n",$linesScriptCA),
             "scriptSite" => implode("\n",$linesScriptSite),
             "ext"        => implode("\n",$linesExt),
-            "vhost"      => implode("\n",$linesVhosts)
+            "vhost"      => implode("\n",$linesVhosts),
+            "info"       => implode("\n",$linesInfo)
         );
     }
 }
