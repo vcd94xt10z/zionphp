1397d00b07b7d1e8c09ba34dfa8f915b085bbf26
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-05-18 08:27:42 -0300

    .

---
+++ backend/zion/utils/GoogleUtils.class.php
@@ -0,0 +1,27 @@
+<?php 
+namespace zion\utils;
+
+class GoogleUtils {
+    public static function isRecaptchaValid($secretKey,$captcha){
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = array('secret' => $secretKey, 'response' => $captcha);
+        
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+        $responseKeys = json_decode($response,true);
+        header('Content-type: application/json');
+        if($responseKeys["success"]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+?>
