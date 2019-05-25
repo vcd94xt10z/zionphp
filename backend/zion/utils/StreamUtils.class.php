80846c34291ad34f7b15f7b442bb8af434d6f476
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-05-25 09:48:03 -0300

    .

---
+++ backend/zion/utils/StreamUtils.class.php
@@ -0,0 +1,124 @@
+<?php
+namespace zion\utils;
+
+use Exception;
+
+/**
+ * @author Vinicius
+ * @since 25/05/2019
+ */
+class StreamUtils {
+    /**
+     * Envia um arquivo para o cliente por stream
+     * @param string $file
+     * @throws Exception
+     */
+    public static function sendFile($file){
+        if(!file_exists($file)){
+            throw new Exception("O arquivo não existe",1);
+        }
+        
+        $fp = fopen($file, 'r');
+        if($fp === false){
+            throw new Exception("Erro em criar ponteiro para o arquivo",2);
+        }
+        $readBytes = fpassthru($fp);
+        if($readBytes === false){
+            fclose($fp);
+            throw new Exception("Erro em ler dados do arquivo por stream",3);
+        }
+        
+        fclose($fp);
+    }
+    
+    /**
+     * Cria um arquivo codificado por gzip a partir de outro arquivo
+     * @param string $fileSource
+     * @param string $fileDest
+     * @param int $level
+     * @throws Exception
+     * @see https://stackoverflow.com/questions/6073397/how-do-you-create-a-gz-file-using-php
+     */
+    public static function gzipFile($fileSource, $fileDest, $level = 9) {
+        if($fileSource == ""){
+            throw new Exception("O arquivo origem esta vazio",1);
+        }
+        
+        if(!file_exists($fileSource)){
+            throw new Exception("O arquivo origem não existe",2);
+        }
+        
+        if(file_exists($fileDest)){
+            throw new Exception("O arquivo destino já existe, remova-o e tente novamente",3);
+        }
+        
+        $mode = 'wb' . $level;
+        $error = false;
+        if($fp_out = gzopen($fileDest, $mode)) {
+            if($fp_in = fopen($fileSource, 'rb')) {
+                while(!feof($fp_in)) {
+                    gzwrite($fp_out, fread($fp_in, 1024 * 512));
+                }
+                fclose($fp_in);
+            } else {
+                $error = true;
+            }
+            gzclose($fp_out);
+        } else {
+            $error = true;
+        }
+        
+        if($error){
+            throw new Exception("Erro em gzip arquivo",4);
+        }
+    }
+    
+    /**
+     * Baixa um arquivo de uma URL utilizando streams
+     * @param string $url
+     * @param string $file
+     * @param int $timeout
+     * @throws Exception
+     * @see https://stackoverflow.com/questions/6914912/streaming-a-large-file-using-php
+     */
+    public static function downloadFile($url,$file,$timeout=60){
+        $fp = @fopen($file, 'w+');
+        if($fp === false){
+            throw new Exception("Erro em ler arquivo",1);
+        }
+        
+        $ch = curl_init();
+        if($ch === false){
+            fclose($fp);
+            throw new Exception("Erro em iniciar curl",2);
+        }
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $result = curl_exec($ch);
+        if($result === false){
+            curl_close($ch);
+            fclose($fp);
+            throw new Exception("Erro em executar curl",3);
+        }
+        
+        if(curl_errno($ch) == 28) {
+            curl_close($ch);
+            fclose($fp);
+            throw new Exception("Timeout",4);
+        }
+        
+        $curlInfo = curl_getinfo($ch);
+        if(strpos($curlInfo["http_code"],"2") !== 0){
+            curl_close($ch);
+            fclose($fp);
+            throw new Exception("Erro em executar curl, status ".$curlInfo["http_code"],5);
+        }
+        
+        curl_close($ch);
+        fclose($fp);
+    }
+}
+?>
