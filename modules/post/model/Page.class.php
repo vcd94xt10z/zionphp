36bc56dc67898b9b6edea2a6530c2fbef53c6e58
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-06-19 14:10:37 -0300

    .

--- modules/post/model/Page.class.php
+++ modules/post/model/Page.class.php
@@ -1,6 +1,7 @@
 <?php 
 namespace zion\mod\post\model;
 
+use Exception;
 use zion\core\System;
 use zion\utils\HTTPUtils;
 
@@ -9,35 +10,38 @@ use zion\utils\HTTPUtils;
  */
 class Page {
     public static function loadByRewrite(){
-        // input
-        $uri = explode("?",$_SERVER["REQUEST_URI"]);
-        $uri = trim($uri[0],"/");
-        
-        // process
-        $db = System::getConnection();
-        $dao = System::getDAO($db,"zion_post_page");
-        
-        $keys = array(
-            "mandt"   => 0,
-            "rewrite" => $uri,
-            "status"  => "A"
-        );
-        $obj = $dao->getObject($db, $keys);
-        if($obj == null){
-            return;
+        try {
+            // input
+            $uri = explode("?",$_SERVER["REQUEST_URI"]);
+            $uri = trim($uri[0],"/");
+            
+            // process
+            $db = System::getConnection();
+            $dao = System::getDAO($db,"zion_post_page");
+            
+            $keys = array(
+                "mandt"   => 0,
+                "rewrite" => $uri,
+                "status"  => "A"
+            );
+            $obj = $dao->getObject($db, $keys);
+            if($obj == null){
+                return;
+            }
+            
+            // output
+            HTTPUtils::status($obj->get("http_status"));
+            header('Content-Type: text/html; charset=utf-8');
+            header("Cache-Control: max-age=".$obj->get("cache_maxage").", s-maxage=".$obj->get("cache_smaxage"));
+            
+            if($obj->get("use_template") == 1){
+                echo $obj->get("content_html");
+            }else{
+                echo $obj->get("content_html");
+            }
+            exit();
+        }catch(Exception $e){
         }
-        
-        // output
-        HTTPUtils::status($obj->get("http_status"));
-        header('Content-Type: text/html; charset=utf-8');
-        header("Cache-Control: max-age=".$obj->get("cache_maxage").", s-maxage=".$obj->get("cache_smaxage"));
-        
-        if($obj->get("use_template") == 1){
-            echo $obj->get("content_html");
-        }else{
-            echo $obj->get("content_html");
-        }
-        exit();
     }
 }
 ?>
