ce5016cde1cb1fb62c65fee1e4183daee1f3cca1
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-06-21 14:18:35 -0300

    .

--- modules/monitor/index.php
+++
@@ -1,3 +0,0 @@
-<?php 
-header("Location: /zion/mod/monitor/Object/list");
-?>
--- modules/monitor/view/js/object-list.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	var code = "";
-	code += "<a href='/zion/mod/monitor/Object/monitor' class='btn btn-outline-secondary'>Monitor</a>"
-	$("#button-new").parent().append(code);
-});
