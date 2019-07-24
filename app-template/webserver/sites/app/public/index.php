e8d191d8fb7148860d24ba381f869a60521bd8be
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-07-24 13:28:06 -0300

    .

--- app-template/webserver/sites/app/public/index.php
+++ app-template/webserver/sites/app/public/index.php
@@ -5,10 +5,21 @@ require(dirname(dirname(dirname(__FILE__)))."/zionphp/autoload.php");
 
 \zion\core\App::route();
 
-// teste
-HTTPUtils::status(200);
-echo "Hello world";
+// exemplo de URL amigável
+if(strpos($_SERVER["REQUEST_URI"],"/minha-url-amigavel") === 0){
+    //$ctrl = new \mod\core\controller\UserController();
+    //$ctrl->actionTest();
+    exit();
+}
 
+// index
+if($_SERVER["REQUEST_URI"] == "/"){
+    HTTPUtils::status(200);
+    echo "Hello world";
+    exit();
+}
+
+// se chegou até aqui, nenhuma URL encontrada
 if(!headers_sent()){
     HTTPUtils::status(404);
     HTTPUtils::template(404);
