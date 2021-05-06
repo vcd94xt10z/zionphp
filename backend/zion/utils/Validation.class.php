4dee9684196b9266335ea185b8d3395c902958a2
Vinicius <vcd94xt10z@users.noreply.github.com>
2021-05-06 14:38:37 -0300

    Update Validation.class.php

--- backend/zion/utils/Validation.class.php
+++ backend/zion/utils/Validation.class.php
@@ -58,7 +58,7 @@ class Validation {
                 $j = ($j == 2) ? 9 : $j - 1;
             }
             $resto = $soma % 11;
-            if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto))
+            if ($cnpj[12] != ($resto < 2 ? 0 : 11 - $resto))
                 return false;
                 // Valida segundo dígito verificador
                 for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++)
